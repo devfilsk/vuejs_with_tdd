f00bf4da7b33bf14f3fe79a7ebecd15ad4e1b486
Filipe Maciel <devfilsk@gmail.com>
2019-11-18 01:02:08 -0300

    Card to show user of github

---
+++ src/components/CardToggle.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container">
+      <div class="card-img">
+            <img :src="user.avatar_url" alt="">
+      </div>
+      <div class="card-header">
+            <p>{{this.user.name}}</p>
+            <p v-on:click="open()">arrow</p>
+      </div>
+      <div class="card-body" v-bind:class="[isOpen ? 'active' : 'inactive']">
+          <div class="card-row">
+              <p>Bio:</p>
+              <p>{{ user.bio }}</p>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'CardToggle',
+    data() {
+        return {
+            isOpen: false
+        }
+    },
+    props: {
+        user: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        open() {
+            this.isOpen = !this.isOpen
+            console.log("OPEN")
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.container {
+    width: 350px;
+    min-height: 150px;
+    border-radius: 4px;
+    background: #FFF;
+    overflow: hidden;
+    flex-direction: column;
+    img {
+        width: 100%;
+        height: auto;
+    }
+}
+.card-img {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+}
+.card-header {
+    color: #353535;
+    font-weight: 600;
+    display: flex;
+    font-size: 20px;
+    padding: 10px;
+    justify-content: space-between;
+}
+.card-body {
+    transition: all .3s;
+    display: flex;
+    overflow: hidden;
+    flex-direction: column;
+    position: relative;
+}
+.card-row {
+    display: flex;
+    flex-direction: row;
+    padding: 10px;  
+    position: absolute;
+    top: 0;
+    right: 0;
+}
+.active {
+    min-height: 100px;
+}
+.inactive {
+    min-height: 0;
+}
+</style>
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -2,22 +2,31 @@
 import VUserSearchForm from '@/components/VUserSearchForm';
 import VUserProfile from '@/components/VUserProfile';
 import { mapState } from 'vuex';
-
+import CardToggle from '@/components/CardToggle';
 export default {
     name: "UserView",
     components: {
       VUserSearchForm,
-      VUserProfile
+      VUserProfile,
+      CardToggle,
+    },
+    data(){
+      return {
+        searched: []
+      }
     },
     methods: {
       searchUser(username) {
         this.$store.dispatch('SEARCH_USER', { username })
+      },
+      show(){
+        console.log(user)
       }
     },
     computed: {
       ...mapState({
         user: 'user',
-      })
+      }),
     }
 }
 </script>
@@ -28,6 +37,7 @@ export default {
         @submitted="searchUser" 
       />
       <VUserProfile :user="user"/>
+      <CardToggle v-if="Object.values(user).length > 0" :user="user"/>
   </div>
 </template>
 
